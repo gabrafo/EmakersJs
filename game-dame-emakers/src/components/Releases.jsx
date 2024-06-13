6b77777378afcc3522b1d8b91b467bb09b97c8e5
gabrafo <gafagundesmesquita@gmail.com>
2024-06-13 20:40:07 -0300

    HomePage completed

--- game-dame-emakers/src/components/Releases.jsx
+++ game-dame-emakers/src/components/Releases.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import "../styles/components/Releases.css"
+import bobesponja from "../assets/bobesponja.png"
+import deliverus from "../assets/deliver.png"
+import spellforce from "../assets/spellforce.png"
+import deathstranding from "../assets/loveukojima.png"
+import perish from '../assets/perish.png'
+import cart from '../assets/cart.png'
 
 const Releases = () => {
   return (
     <div className="releases_panel">
-        asasas
+        <p id='releases_txt'>Lançamentos</p>
+        <img src={bobesponja} alt="SpongeBob Squarepants: Battle for Bikini Bottom" id='bobimg'/>
+        <img src={deliverus} alt="Deliver Us Mars" id='deliverusimg'/>
+        <img src={spellforce} alt="Spellforce 3" id='spellforceimg'/>
+        <img src={deathstranding} alt="Death Stranding" id='deathimg'/>
+        <img src={perish} alt="Perish" id='perishimg'/>
+        <div className='price_bobesponja'>
+          R$ 299,00
+        </div>
+        <div className='price_deliverus'>
+          R$ 299,00
+        </div>
+        <div className='price_spellforce'>
+          R$ 299,00
+        </div>
+        <div className='price_death'>
+          R$ 299,00
+        </div>
+        <div className='price_perish'>
+          R$ 299,00
+        </div>
+        <div className='shopping_cart'>
+          <img src={cart} alt="" />
+        </div>
     </div>
   )
 }
--- game-dame-emakers/src/pages/HomePage.jsx
+++ game-dame-emakers/src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import "../styles/pages/HomePage.css"
 import NavBar from "../components/NavBar";
 import Carousel from '../components/Carousel';
 import Releases from "../components/Releases";
 
 const HomePage = () => {
   return (
-    <div className='full_panel'>
+    <div className='full_homepage_panel'>
       <NavBar />
       <Carousel />
       <Releases />
